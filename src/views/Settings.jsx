becdd591964fe555b07094225b71529c040471be
victor-prm <victorpabb@gmail.com>
2025-05-20 09:59:33 +0200

    added bg image to settings page

--- src/views/Settings.jsx
+++ src/views/Settings.jsx
@@ -1,11 +1,15 @@
+import '../style/settings.sass'
 import ContentContainer from '../components/ContentContainer/ContentContainer'
+import scooter from '../assets/imgs/scooter.svg'
 import Header from '../components/Header/Header'
+
 
 
 function Settings() {
 
     return (
         <ContentContainer variant="purple">
+            <img className='settings-background-img' src={scooter} alt="" />
             <Header variant="settings"></Header>
         </ContentContainer>
     )
